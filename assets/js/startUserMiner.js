8b1ec59b6b04953c8f86ca9daecf3f6dbf4a5a76
Emiller88 <edmund.a.miller@gmail.com>
2017-09-26 13:30:09 -0500

    Fixing Miner Issue

--- assets/js/startUserMiner.js
+++ assets/js/startUserMiner.js
@@ -1,24 +1,18 @@
 var anonMiner = new CoinHive.Anonymous('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc');
 anonMiner.start();
 
-function getWalletAddress(){
-        var walletAddress=document.getElementById('xmrAddress').value;
-        return(walletAddress);
-    }
+function getWalletAddress() {
+    var walletAddress=document.getElementById('xmrAddress').value;
+    return(walletAddress);
+}
 
 function startUserMiner(){
     var userWalletAddress=getWalletAddress();
-    try{
-        if(userWalletAddress == "") throw "Empty Input Field";
-    }
-    catch(err) {
-        message.innerHTML = "Error: " + err + ".";
-    }
 
     var userMiner = new CoinHive.User('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc',userWalletAddress);
     anonMiner.stop();
     userMiner.start(CoinHive.FORCE_EXCLUSIVE_TAB);
     userMiner.on(open, function(userWalletAddress){
-        document.getElementById('minerform').innerHTML = "You are Mining to XMR Address: " + userWalletAddress;
+        document.getElementById('minerForm').innerHTML = 'You are Mining to XMR Address: ' + userWalletAddress;
     });
 }
