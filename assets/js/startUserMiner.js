a3aa5482591a40403757629bbcd4d7d1469367a1
Emiller88 <edmund.a.miller@gmail.com>
2017-09-27 13:53:50 -0500

    Update Alert

--- assets/js/startUserMiner.js
+++ assets/js/startUserMiner.js
@@ -12,7 +12,7 @@ function startUserMiner(){
     var userMiner = new CoinHive.User('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc',userWalletAddress);
     anonMiner.stop();
     userMiner.start();
-    alert('You are now mining to: "' + userWalletAddress + '" Please leave this page open, the faucet will be dripping')
+    alert('You are now mining to: "' + userWalletAddress + '" Please leave this page open, the faucet will be dripping. You can close this Alert')
     userMiner.on(open, function(userWalletAddress){
         document.getElementById('minerForm').innerHTML = 'You are Mining to XMR Address: ' + userWalletAddress;
     });
