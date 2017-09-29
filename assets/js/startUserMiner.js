6a3df4a935f847105707712edd1eaa4d0aac3e27
Emiller88 <edmund.a.miller@gmail.com>
2017-09-29 18:52:42 -0500

    Update Work

--- assets/js/startUserMiner.js
+++ assets/js/startUserMiner.js
@@ -1,38 +1,10 @@
-var anonMiner = new CoinHive.Anonymous('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc');
-anonMiner.start();
+//var anonMiner = new CoinHive.Anonymous('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc');
+//anonMiner.start();
 
 function getWalletAddress() {
     var walletAddress=document.getElementById('xmrAddress').value;
     return(walletAddress);
 }
-
-function minerStats(currentWallet){
-    document.getElementById('minerForm').innerHTML =
-\        "<h3 style="text-align:center;color:4c4c4c;" id="formTitle">
-\            Miner Stats
-\        </h3>
-\       <form class="register-form">
-\           <div class="tim-title">
-\               <h3>Wallet Address:
-\                   <div id="statAddress"></div>
-\                   <br/>
-\                   Hashrate:
-\                  <div id="statHashRate"></div>
-\                  <br/>
-\                  Session Hashes:
-\                   <div id="statsSesHash"></div>
-\                   <br/>
-\                   Total Hashes:
-\                   <div id="statsAcceptHash"></div>
-\                   <br/>
-\           </div>
-\       <form>";
-    document.getElementById('statAddress').innerHTML = currentWallet;
-    setInterval(function(){document.getElementById('statHashRate').innerHTML = userMiner.getHashesPerSecond() + ' H/s';},10000);
-    setInterval(function(){document.getElementById('statsSesHash').innerHTML = userMiner.getTotalHashes([interpolate]) + ' Hashes';},50);
-    setInterval(function(){document.getElementById('statsAcceptHash').innerHTML = userMiner.getAcceptedHashes() + ' Hashes';},5000);
-}
-
 function startUserMiner(){
     var userWalletAddress=getWalletAddress();
 
@@ -40,5 +12,4 @@ function startUserMiner(){
     anonMiner.stop();
     userMiner.start();
     alert('You are now mining to: "' + userWalletAddress + '" Please leave this page open, the faucet will be dripping. You can close this Alert');
-    userMiner.on('open', function minerStats(userWalletAddress));
 }
