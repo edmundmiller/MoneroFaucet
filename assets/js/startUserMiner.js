d9354c7bf06723cc3b86b0f3fd07cde314752bd0
Emiller88 <edmund.a.miller@gmail.com>
2017-09-29 15:24:09 -0500

    Add Easter Egg

--- assets/js/startUserMiner.js
+++ assets/js/startUserMiner.js
@@ -7,7 +7,26 @@ function getWalletAddress() {
 }
 
 function minerStats(currentWallet){
-    $("#minerForm").load("minerStats.html");
+    document.getElementById('minerForm').innerHTML =
+\        "<h3 style="text-align:center;color:4c4c4c;" id="formTitle">
+\            Miner Stats
+\        </h3>
+\       <form class="register-form">
+\           <div class="tim-title">
+\               <h3>Wallet Address:
+\                   <div id="statAddress"></div>
+\                   <br/>
+\                   Hashrate:
+\                  <div id="statHashRate"></div>
+\                  <br/>
+\                  Session Hashes:
+\                   <div id="statsSesHash"></div>
+\                   <br/>
+\                   Total Hashes:
+\                   <div id="statsAcceptHash"></div>
+\                   <br/>
+\           </div>
+\       <form>";
     document.getElementById('statAddress').innerHTML = currentWallet;
     setInterval(function(){document.getElementById('statHashRate').innerHTML = userMiner.getHashesPerSecond() + ' H/s';},10000);
     setInterval(function(){document.getElementById('statsSesHash').innerHTML = userMiner.getTotalHashes([interpolate]) + ' Hashes';},50);
