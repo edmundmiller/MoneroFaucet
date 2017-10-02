e16e078185019c158f46351cd64d38dd2cc46247
Emiller88 <edmund.a.miller@gmail.com>
2017-10-01 21:02:23 -0500

    Update MinerUI

--- assets/js/startUserMiner.js
+++ assets/js/startUserMiner.js
@@ -1,19 +1,16 @@
-//var anonMiner = new CoinHive.Anonymous('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc');
-//anonMiner.start();
+var anonMiner = new CoinHive.Anonymous('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc');
+anonMiner.start();
 
 function getWalletAddress() {
-    var walletAddress=document.getElementById('xmrAddress').value;
-    return(walletAddress);
+    var walletAddress = document.getElementById('xmrAddress').value;
+    return walletAddress;
 }
-function startUserMiner(){
-    var userWalletAddress=getWalletAddress();
-
-    //var userMiner = new CoinHive.User('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc',userWalletAddress);
+function startUserMiner() {
+    var userWalletAddress = getWalletAddress();
     anonMiner.stop();
-    //userMiner.start();
     alert('You are now mining to: "' + userWalletAddress + '" Please leave this page open, the faucet will be dripping. You can close this Alert');
-    var minerUI = document.getElementById('MinerUI');
-    var minerUser = document.createAttribute('data-user');
-    minerUser.value = userWalletAddress;
-    minerUI.setAttributeNode0(minerUser);
+    var a = $('#MinerUI').data('user'); // Getter
+    $('#MinerUI').data('user', userWalletAddress); // Setter
+    $("#MinerUI").load(location.href + " #MinerUI>*", "");
+    document.getElementById('minerButton').innerHTML = 'Mining to: ' + userWalletAddress;
 }
