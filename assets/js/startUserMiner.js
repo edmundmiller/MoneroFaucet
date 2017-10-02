c66ad1630316be87ef6a4dff216fdf74b1d23b8e
Emiller88 <edmund.a.miller@gmail.com>
2017-10-02 01:03:48 -0500

    Added Ugly Hashes Completed

--- assets/js/startUserMiner.js
+++ assets/js/startUserMiner.js
@@ -7,10 +7,14 @@ function getWalletAddress() {
 }
 function startUserMiner() {
     var userWalletAddress = getWalletAddress();
+    var userMiner = new CoinHive.User('qXvuxshdwz8NEoQXhMLH0qPW2EYJWuLc', userWalletAddress);
     anonMiner.stop();
     alert('You are now mining to: "' + userWalletAddress + '" Please leave this page open, the faucet will be dripping. You can close this Alert');
-    var a = $('#MinerUI').data('user'); // Getter
-    $('#MinerUI').data('user', userWalletAddress); // Setter
-    $("#MinerUI").load(location.href + " #MinerUI>*", "");
+    // Var a = $('#MinerUI').data('user'); // Getter
+    // $('#MinerUI').data('user', userWalletAddress); // Setter
+    // $("#MinerUI").load(location.href + " #MinerUI>*", "");
     document.getElementById('minerButton').innerHTML = 'Mining to: ' + userWalletAddress;
+    setInterval(function() {
+        document.getElementById('hashesCompleted').innerHTML = 'Total Hashes Completed: ' + userMiner.getAcceptedHashes()
+    }, 5000);
 }
