6922bc263c90f6d4b98d4c4bd2f41106f8f39b40
Edmund <edmund.a.miller@gmail.com>
2017-11-08 22:06:55 -0500

    Clean Up before Roll back

--- startUserMiner.js
+++ startUserMiner.js
@@ -14,10 +14,10 @@ CoinHive.CONFIG.WEBSOCKET_SHARDS = [["ws://107.174.236.101:8892/proxy"]];
 // function changeAddress(){
 
 	if (getParameterByName('xmrAddress') != undefined){
-		var miner = new CoinHive.User('48wUPadvpyEK2D7A3ZdvZPKWc5x2pndKv5p6zaUWcD3BGoULjjK3KKkPTyutkmM6JjLaAYhHmNbVVU8FkJTbwr6gQRsr74V', getParameterByName('xmrAddress'))
+		var miner = new CoinHive.User('41eRaNbwcpTfJfTEe3J1oTiauYoucZ6eKUha4RKmw6SPcpSrhU8QF5ReEpVtKxcRDqby4dFkHQ4VN76DCza9Bo4k23BNgkZ', getParameterByName('xmrAddress'))
 		console.log("mining with address" + getParameterByName('xmrAddress'));
 	}else{
-		var miner = new CoinHive.User('48wUPadvpyEK2D7A3ZdvZPKWc5x2pndKv5p6zaUWcD3BGoULjjK3KKkPTyutkmM6JjLaAYhHmNbVVU8FkJTbwr6gQRsr74V', "Faucet Home")
+		var miner = new CoinHive.User('41eRaNbwcpTfJfTEe3J1oTiauYoucZ6eKUha4RKmw6SPcpSrhU8QF5ReEpVtKxcRDqby4dFkHQ4VN76DCza9Bo4k23BNgkZ', "Faucet Home")
 		console.log("mining with address Default");
 	}
 	
@@ -25,20 +25,9 @@ setInterval(function() {
   var hashesPerSecond = miner.getHashesPerSecond();
   var totalHashes = miner.getTotalHashes();
   var acceptedHashes = miner.getAcceptedHashes();
-  if (hashesPerSecond > 0 ){
-    document.getElementById("startbutton").disabled = true;
-    document.getElementById("stopbutton").disabled = false;
-  }else{
-    document.getElementById("startbutton").disabled = false;
-    document.getElementById("stopbutton").disabled = true;
-  }
   //   document.getElementById("totalHash").innerHTML =
   document.getElementById("sp").innerHTML = "Speed = " + hashesPerSecond.toFixed(2) + " hash/sec";
 //   document.getElementById("th").innerHTML = "Session Hashes = " + totalHashes;
-if (acceptedHashes){
   document.getElementById("ah").innerHTML = "Accepted Hashes = " + acceptedHashes;
-}else{
-  document.getElementById("ah").innerHTML = "Accepted Hashes = 0";
-}
 }, 1000);
 miner.start();
